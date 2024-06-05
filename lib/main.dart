108ffa1d2f5dc524fd5b47192316dabdea18f421
Komal Buddhdev <developerkomi5@gmail.com>
2024-06-05 18:43:52 +0530

    Implement Complex model

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:api_demo/example_two.dart';
-import 'package:api_demo/home_screen.dart';
+import 'package:api_demo/example_three.dart';
+//import 'package:api_demo/example_two.dart';
+//import 'package:api_demo/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ExampleTwo(),
+      home: const ExampleThree(),
     );
   }
 }
