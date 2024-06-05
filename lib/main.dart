fc144f2b44bca3b213b233703f7e4bcb5bea3b01
Komal Buddhdev <developerkomi5@gmail.com>
2024-06-05 19:03:13 +0530

    Direct creating model

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:api_demo/example_three.dart';
+//import 'package:api_demo/example_three.dart';
 //import 'package:api_demo/example_two.dart';
 //import 'package:api_demo/home_screen.dart';
+import 'package:api_demo/example_four.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ExampleThree(),
+      home: const ExampleFour(),
     );
   }
 }
