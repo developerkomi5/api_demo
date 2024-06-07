11ef0ce2ffc7a456e9ccf73e5b069a1c26227df7
Komal Buddhdev <developerkomi5@gmail.com>
2024-06-07 16:12:32 +0530

    Build Login & Signup API with POST method

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 //import 'package:api_demo/example_three.dart';
 //import 'package:api_demo/example_two.dart';
 //import 'package:api_demo/home_screen.dart';
-import 'package:api_demo/example_five.dart';
+//import 'package:api_demo/example_five.dart';
+import 'package:api_demo/signup.dart';
 //import 'package:api_demo/example_four.dart';
 import 'package:flutter/material.dart';
 
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LastExampleScreen(),
+      home: const SignUpScreen(),
     );
   }
 }
